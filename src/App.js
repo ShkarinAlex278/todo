f5e93272b987eba130721d65a00b29dbe07e0515
AlexShkarin <shkarinalex278@gmail.com>
2025-09-01 21:04:55 -0400

    Add Style Buttoms

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
+function sayHello() {
+      alert('Hello Rohit!');
+    }
+
 
 function App() {
   return (
   <div className='form'>
 
-  <div class="wrapper">
+  <div className="wrapper">
     <header>Todo App</header>
-    <div class="inputField">
+    <div className="inputField">
       <input type="text" placeholder="Add your new todo" />
-      <button><i class="fas fa-plus"></i></button>
+      <button className='add' onClick={sayHello}> Add
+         {/* <i class="fas fa-plus"></i> */}
+        </button>
     </div>
-    <ul class="todoList">
+    <ul className="todoList">
     </ul>
-    <div class="footer">
-      <span>You have <span class="pendingTasks"></span> pending tasks</span>
-      <button>Clear All</button>
+    <div className="footer">
+      <span>You have <span className="pendingTasks"></span> pending tasks</span>
+      <button onClick={() => {console.log("Click Clear...")}}>Clear All</button>
     </div>
   </div>
 
